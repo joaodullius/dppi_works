f802766d911db270f58bd55a868952b80b44837d
João Dullius <joaodullius@bpmrep.com.br>
2025-06-30 20:10:51 -0300

    Refactor main.c: reorganize includes, improve input/output pin configuration, and enhance DPPI setup

--- gpiote_dppi_gpiote/src/main.c
+++ gpiote_dppi_gpiote/src/main.c
@@ -5,12 +5,12 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/irq.h>
 
 #include <nrfx_gpiote.h>
 #include <helpers/nrfx_gppi.h>
 
-#include <zephyr/logging/log.h>
-#include <zephyr/irq.h>
 LOG_MODULE_REGISTER(nrfx_sample, LOG_LEVEL_INF);
 
 #define INPUT_PIN	NRF_DT_GPIOS_TO_PSEL(DT_ALIAS(sw0), gpios)
@@ -24,12 +24,109 @@ BUILD_ASSERT(NRF_DT_GPIOTE_INST(DT_ALIAS(led0), gpios) == GPIOTE_INST,
 BUILD_ASSERT(IS_ENABLED(_CONCAT(CONFIG_, _CONCAT(NRFX_GPIOTE, GPIOTE_INST))),
 	"NRFX_GPIOTE" STRINGIFY(GPIOTE_INST) " must be enabled in Kconfig");
 
-
 static void button_handler(nrfx_gpiote_pin_t pin,
 			   nrfx_gpiote_trigger_t trigger,
 			   void *context)
 {
 	LOG_INF("GPIO input event callback");
+}
+
+static nrfx_err_t configure_input_pin(const nrfx_gpiote_t * p_gpiote,
+					nrfx_gpiote_pin_t input_pin,
+					uint8_t *p_in_channel)
+{
+	nrfx_err_t err;
+
+	/* Allocate a channel for the input pin */
+	err = nrfx_gpiote_channel_alloc(p_gpiote, p_in_channel);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("Failed to allocate in_channel, error: 0x%08X", err);
+		return err;
+	}
+
+	static const nrf_gpio_pin_pull_t pull_config = NRF_GPIO_PIN_PULLUP;
+	nrfx_gpiote_trigger_config_t trigger_config = {
+		.trigger = NRFX_GPIOTE_TRIGGER_HITOLO,
+		.p_in_channel = p_in_channel,
+	};
+	static const nrfx_gpiote_handler_config_t handler_config = {
+		.handler = button_handler,
+	};
+	nrfx_gpiote_input_pin_config_t input_config = {
+		.p_pull_config    = &pull_config,
+		.p_trigger_config = &trigger_config,
+		.p_handler_config = &handler_config
+	};
+
+	err = nrfx_gpiote_input_configure(p_gpiote, input_pin, &input_config);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("nrfx_gpiote_input_configure error: 0x%08X", err);
+		return err;
+	}
+
+	/* Enable the input trigger */
+	nrfx_gpiote_trigger_enable(p_gpiote, input_pin, true);
+	return NRFX_SUCCESS;
+}
+
+static nrfx_err_t configure_output_pin(const nrfx_gpiote_t * p_gpiote,
+					 nrfx_gpiote_pin_t output_pin,
+					 uint8_t *p_out_channel)
+{
+	nrfx_err_t err;
+
+	/* Allocate a channel for the output pin */
+	err = nrfx_gpiote_channel_alloc(p_gpiote, p_out_channel);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("Failed to allocate out_channel, error: 0x%08X", err);
+		return err;
+	}
+
+	static const nrfx_gpiote_output_config_t output_config = {
+		.drive         = NRF_GPIO_PIN_S0S1,
+		.input_connect = NRF_GPIO_PIN_INPUT_DISCONNECT,
+		.pull          = NRF_GPIO_PIN_NOPULL,
+	};
+	const nrfx_gpiote_task_config_t task_config = {
+		.task_ch  = *p_out_channel,
+		.polarity = NRF_GPIOTE_POLARITY_TOGGLE,
+		.init_val = 1,
+	};
+
+	err = nrfx_gpiote_output_configure(p_gpiote, output_pin, &output_config,
+					   &task_config);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("nrfx_gpiote_output_configure error: 0x%08X", err);
+		return err;
+	}
+
+	/* Enable the output task */
+	nrfx_gpiote_out_task_enable(p_gpiote, output_pin);
+	return NRFX_SUCCESS;
+}
+
+static nrfx_err_t configure_dppi(const nrfx_gpiote_t * p_gpiote,
+				 nrfx_gpiote_pin_t input_pin,
+				 nrfx_gpiote_pin_t output_pin)
+{
+	nrfx_err_t err;
+	uint8_t ppi_channel;
+
+	/* Allocate a DPPI channel */
+	err = nrfx_gppi_channel_alloc(&ppi_channel);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("nrfx_gppi_channel_alloc error: 0x%08X", err);
+		return err;
+	}
+
+	/* Setup endpoints so that the input pin event triggers the output pin task */
+	nrfx_gppi_channel_endpoints_setup(ppi_channel,
+		nrfx_gpiote_in_event_address_get(p_gpiote, input_pin),
+		nrfx_gpiote_out_task_address_get(p_gpiote, output_pin));
+
+	/* Enable the DPPI channel */
+	nrfx_gppi_channels_enable(BIT(ppi_channel));
+	return NRFX_SUCCESS;
 }
 
 int main(void)
@@ -38,102 +135,33 @@ int main(void)
 
 	nrfx_err_t err;
 	uint8_t in_channel, out_channel;
-	uint8_t ppi_channel;
 	const nrfx_gpiote_t gpiote = NRFX_GPIOTE_INSTANCE(GPIOTE_INST);
 
-	/* Connect GPIOTE instance IRQ to irq handler */
+	/* Connect GPIOTE instance IRQ to the interrupt handler */
 	IRQ_CONNECT(DT_IRQN(GPIOTE_NODE), DT_IRQ(GPIOTE_NODE, priority), nrfx_isr,
-		    NRFX_CONCAT(nrfx_gpiote_, GPIOTE_INST, _irq_handler), 0);
+			NRFX_CONCAT(nrfx_gpiote_, GPIOTE_INST, _irq_handler), 0);
 
-	/* Initialize GPIOTE (the interrupt priority passed as the parameter
-	 * here is ignored, see nrfx_glue.h).
-	 */
 	err = nrfx_gpiote_init(&gpiote, 0);
 	if (err != NRFX_SUCCESS) {
 		LOG_ERR("nrfx_gpiote_init error: 0x%08X", err);
 		return 0;
 	}
 
-	err = nrfx_gpiote_channel_alloc(&gpiote, &in_channel);
+	err = configure_input_pin(&gpiote, INPUT_PIN, &in_channel);
 	if (err != NRFX_SUCCESS) {
-		LOG_ERR("Failed to allocate in_channel, error: 0x%08X", err);
 		return 0;
 	}
 
-	err = nrfx_gpiote_channel_alloc(&gpiote, &out_channel);
+	err = configure_output_pin(&gpiote, OUTPUT_PIN, &out_channel);
 	if (err != NRFX_SUCCESS) {
-		LOG_ERR("Failed to allocate out_channel, error: 0x%08X", err);
 		return 0;
 	}
 
-	/* Initialize input pin to generate event on high to low transition
-	 * (falling edge) and call button_handler()
-	 */
-	static const nrf_gpio_pin_pull_t pull_config = NRF_GPIO_PIN_PULLUP;
-	nrfx_gpiote_trigger_config_t trigger_config = {
-		.trigger = NRFX_GPIOTE_TRIGGER_HITOLO,
-		.p_in_channel = &in_channel,
-	};
-	static const nrfx_gpiote_handler_config_t handler_config = {
-		.handler = button_handler,
-	};
-	nrfx_gpiote_input_pin_config_t input_config = {
-		.p_pull_config = &pull_config,
-		.p_trigger_config = &trigger_config,
-		.p_handler_config = &handler_config
-	};
-
-	err = nrfx_gpiote_input_configure(&gpiote, INPUT_PIN, &input_config);
-
+	err = configure_dppi(&gpiote, INPUT_PIN, OUTPUT_PIN);
 	if (err != NRFX_SUCCESS) {
-		LOG_ERR("nrfx_gpiote_input_configure error: 0x%08X", err);
 		return 0;
 	}
 
-	/* Initialize output pin. SET task will turn the LED on,
-	 * CLR will turn it off and OUT will toggle it.
-	 */
-	static const nrfx_gpiote_output_config_t output_config = {
-		.drive = NRF_GPIO_PIN_S0S1,
-		.input_connect = NRF_GPIO_PIN_INPUT_DISCONNECT,
-		.pull = NRF_GPIO_PIN_NOPULL,
-	};
-	const nrfx_gpiote_task_config_t task_config = {
-		.task_ch = out_channel,
-		.polarity = NRF_GPIOTE_POLARITY_TOGGLE,
-		.init_val = 1,
-	};
-	err = nrfx_gpiote_output_configure(&gpiote, OUTPUT_PIN,
-					   &output_config,
-					   &task_config);
-	if (err != NRFX_SUCCESS) {
-		LOG_ERR("nrfx_gpiote_output_configure error: 0x%08X", err);
-		return 0;
-	}
-
-	nrfx_gpiote_trigger_enable(&gpiote, INPUT_PIN, true);
-	nrfx_gpiote_out_task_enable(&gpiote, OUTPUT_PIN);
-
-	LOG_INF("nrfx_gpiote initialized");
-
-	/* Allocate a (D)PPI channel. */
-	err = nrfx_gppi_channel_alloc(&ppi_channel);
-	if (err != NRFX_SUCCESS) {
-		LOG_ERR("nrfx_gppi_channel_alloc error: 0x%08X", err);
-		return 0;
-	}
-
-	/* Configure endpoints of the channel so that the input pin event is
-	 * connected with the output pin OUT task. This means that each time
-	 * the button is pressed, the LED pin will be toggled.
-	 */
-	nrfx_gppi_channel_endpoints_setup(ppi_channel,
-		nrfx_gpiote_in_event_address_get(&gpiote, INPUT_PIN),
-		nrfx_gpiote_out_task_address_get(&gpiote, OUTPUT_PIN));
-
-	/* Enable the channel. */
-	nrfx_gppi_channels_enable(BIT(ppi_channel));
-
-	LOG_INF("(D)PPI configured, leaving main()");
+	LOG_INF("nrfx_gpiote and DPPI configured, leaving main()");
 	return 0;
 }
